4440de8f3b35abf6dc7dde3195804e4c40b29ea7
Evin Revello <evin.revello@gmail.com>
2021-07-20 09:18:43 -0700

    apod-scrapper updates and package testing

--- NASA-to-Background.sh
+++ NASA-to-Background.sh
@@ -1,29 +1,30 @@
 #! /bin/bash
 
-# The goal of this script is to automatically save the NASA APOD to a lock/background.
-# get your own NASA API Key: https://api.nasa.gov/
+# The goal of this script is to automatically save the NASA APOD to a lock/background as part of an installable package.
 
-# To install
-    # for all users at login: /etc/profile
-    # for one user at login: .profile
-    
-# Errors can be found: ~/.xsession-errors
+# wget $(curl https://api.nasa.gov/planetary/apod?api_key=$NASA_API_KEY -H "Accept: application/json" -is | grep -oP 'https://.*?\.jpg' | head -n1) --output-document=/usr/share/backgrounds/APOD.jpg 2>/dev/null
+# not every user will implement the API and it is not needed since the URL is publically accessible
 
-# To run when new terminal is launched: /etc/bash.bashrc or .bashrc
+touch /etc/network/if-up.d/NASA_APOD_scrapper
+echo "wget $(echo "https://apod.nasa.gov/apod/""$(curl https://apod.nasa.gov/apod/astropix.html -is | grep -oP '.*?\.jpg' | head -n1 | sed 's/<a href="//' | tr -d ' ')") --output-document=/usr/share/backgrounds/APOD.jpg 2>/dev/null" > /etc/network/if-up.d/NASA_APOD_scrapper
+chmod +x /etc/network/if-up.d/NASA_APOD_scrapper
 
-# After running the command once
+#download todays image 
+wget $(echo "https://apod.nasa.gov/apod/""$(curl https://apod.nasa.gov/apod/astropix.html -is | grep -oP '.*?\.jpg' | head -n1 | sed 's/<a href="//' | tr -d ' ')") --output-document=/usr/share/backgrounds/APOD.jpg 2>/dev/null
 
-# Open this file  /usr/share/gnome-shell/theme/ubuntu.css  
+# To make the APOD a selectable background: /usr/share/gnome-background-properties/ubuntu-wallpapers.xml
 
-# to change the lock screen, edit these lines: #lockDialogGroup {
-#  background: #2c001e url(file:////home/<user-name>/Pictures/lock-background.jpg);
-#  background-repeat: no-repeat;
-#  background-size: cover;
-#  background-position: center; }
+# Replace the last instance/line with
+#</wallpapers>
+# TODO: Dont be this lazy
+# deleting the last line
+sed -i '$ d' /usr/share/gnome-background-properties/ubuntu-wallpapers.xml
+# with this
+echo '<wallpaper>\n<name>NASA APOD</name>\n<filename>/usr/share/backgrounds/APOD.jpg</filename>\n<options>zoom</options>\n<pcolor>#333333</pcolor>\n<scolor>#333333</scolor>\n<shade_type>solid</shade_type>\n</wallpaper>\n</wallpapers>' >> /usr/share/gnome-background-properties/ubuntu-wallpapers.xml
 
-# To change the background: 
-# gsettings set org.gnome.desktop.background picture-uri "/home/<your-user-name>/Pictures/lock-background.jpg"
+# this is the same as setting them in the control panel
+gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/APOD.jpg
+gsettings set org.gnome.desktop.screensaver picture-uri file:////usr/share/backgrounds/APOD.jpg
 
-# Or go into your settings and get it all worked out in some combination
-
-wget $(curl https://api.nasa.gov/planetary/apod?api_key=<your-api-key> -H "Accept: application/json" -is | grep -oP 'https://.*?\.jpg' | head -n1) --output-document=/home/<your-user-name>/Pictures/lock-background.jpg 2>/dev/null
+# gsettings range org.gnome.desktop.background picture-options
+# TODO: check if you need to set the picture options beyond 'zoom'
